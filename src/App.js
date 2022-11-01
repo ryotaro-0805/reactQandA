b1637d71e77ebd9fae3e009274d6fa562440f409
ryotaro-0805 <rasyouryo@yahoo.co.jp>
2022-11-01 18:08:01 +0900

    2022-11-1-完成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const qas = [
+    { q: 'Q1:質問文章が入ります。', a: 'A1：回答文章が入ります。'},
+    { q: 'Q2:質問文章が入ります。', a: 'A2：回答文章が入ります。'},
+    { q: 'Q3:質問文章が入ります。', a: 'A3：回答文章が入ります。'},
+    { q: 'Q4:質問文章が入ります。', a: 'A4：回答文章が入ります。'},
+    { q: 'Q5:質問文章が入ります。', a: 'A5：回答文章が入ります。'}
+  ];
+
+  const clickFnc = (e) => {
+    e.target.nextSibling.className === 'a_class active' ? 
+    e.target.nextSibling.classList.remove('active') : 
+    e.target.nextSibling.classList.add('active');
+  }
+  const clickFnc2 = (e) => {
+    e.target.className === 'a_class active' ? 
+    e.target.classList.remove('active') : 
+    e.target.classList.add('active');
+  }
+
+  const closeFnc=()=>{
+    const getTags=document.querySelectorAll('#pTag');
+    getTags.forEach((getTag)=>{
+      getTag.className==='a_class active' &&
+      getTag.classList.remove('active');
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>"Q" and "A" App</h2>
+      {qas.map((qa, index) => (
+        <div key={index}>
+          <p onClick={clickFnc} className='q-class'>{qa.q}</p>
+          <p id='pTag' onClick={clickFnc2} className='a_class'>{qa.a}</p>
+        </div>
+      ))}
+      <button onClick={closeFnc}>All Close</button>
     </div>
   );
 }
