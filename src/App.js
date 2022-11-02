3a413ca818dec325ac3b2ce71f34ba543ebc899d
ryotaro-0805 <rasyouryo@yahoo.co.jp>
2022-11-02 10:21:08 +0900

    2022-11-2-機能追加中

--- src/App.js
+++ src/App.js
@@ -16,6 +16,14 @@ function App() {
     e.target.nextSibling.classList.remove('active') : 
     e.target.nextSibling.classList.add('active');
   }
+
+  
+  const clickFnc_div= (e) => {
+    e.currentTarget.className === 'wrapper active' ? 
+    e.currentTarget.classList.remove('active') : 
+    e.currentTarget.classList.add('active');
+  }
+
   const clickFnc2 = (e) => {
     e.target.className === 'a_class active' ? 
     e.target.classList.remove('active') : 
@@ -30,16 +38,33 @@ function App() {
     });
   }
 
+  // test
+
+  const testFnc=()=>{
+    test.mainFnc(`running the testCode by ${test.id}`);
+  }
+
+  const test={
+    id:'ryo',
+    mainFnc:(data)=>{
+      console.log(data);
+    }
+  }
+  
+  // test End
+
   return (
     <div className="App">
-      <h2>"Q" and "A" App</h2>
+      {/* <h2>"Q" and "A" App</h2> */}
+      <h2>MAC外注仕様書</h2>
       {qas.map((qa, index) => (
-        <div key={index}>
+        <div onClick={clickFnc_div} className='wrapper' key={index}>
           <p onClick={clickFnc} className='q-class'>{qa.q}</p>
           <p id='pTag' onClick={clickFnc2} className='a_class'>{qa.a}</p>
         </div>
       ))}
       <button onClick={closeFnc}>All Close</button>
+      <button onClick={testFnc} >Test Button</button>
     </div>
   );
 }
